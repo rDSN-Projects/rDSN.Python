04ac63bc6e3e985765bf650e44ffeae8f3f469e4
lishenglong <lishenglong1002@gmail.com>
2015-10-09 10:04:44 +0800

    define linux macro, fix cast bug, tidy get handler function

--- src/dev/python_helper/python_helper.cpp
+++ src/dev/python_helper/python_helper.cpp
@@ -8,6 +8,14 @@
 //# undef _DEBUG
 # include "Python.h"
 
+# pragma once
+# if defined(_WIN32)
+# define EXPORT __declspec(dllexport)
+
+# elif defined(__linux__) || defined(__APPLE__) || defined(__FreeBSD__)
+# define EXPORT __attribute__((visibility("default")))
+# endif
+
 typedef int         dsn_error_t;
 // rDSN allows many apps in the same process for easy deployment and test
 // app ceate, start, and destroy callbacks
@@ -128,22 +136,85 @@ typedef struct
 
 extern "C"
 {
-	__declspec(dllexport) bool dsn_register_app_role_helper(const char* type_name, const char* create, const char* start, dsn_app_destroy destroy);
-	__declspec(dllexport) void dsn_run_helper(int argc, char** argv, bool sleep_after_init);
-	__declspec(dllexport) void dsn_task_call_helper(dsn_task_t task, dsn_task_tracker_t tracker, int delay_milliseconds);
-	__declspec(dllexport) dsn_task_t dsn_task_create_helper(dsn_task_code_t code, void* param, int hash);
-	__declspec(dllexport) dsn_task_t dsn_task_create_timer_helper(dsn_task_code_t code, void* param, int hash, int interval_milliseconds);
-	__declspec(dllexport) uint64_t dsn_address_build_helper(const char* host, int port);
-	__declspec(dllexport) void dsn_rpc_call_helper(uint64_t addr, dsn_task_t rpc_call, dsn_task_tracker_t tracker);
-	__declspec(dllexport) void* dsn_rpc_call_wait_helper(uint64_t addr, dsn_message_t msg, char *ss);
-	__declspec(dllexport) dsn_task_t dsn_rpc_create_response_task_helper(dsn_message_t msg, int param, int reply_hash);
-	__declspec(dllexport) dsn_message_t dsn_msg_create_request_helper(dsn_task_code_t code, int timeout_milliseconds, int request_hash);
-	__declspec(dllexport) dsn_task_code_t dsn_task_code_register_helper(const char* name, int type, int pri, dsn_threadpool_code_t pool);
-	__declspec(dllexport) void marshall_helper(dsn_message_t msg, char* request_content);
-	__declspec(dllexport) bool dsn_rpc_register_handler_helper(dsn_task_code_t code, const char* name, int param);
-	__declspec(dllexport) void marshall_int_msg_helper(int msg_int, char* request_content);
+	EXPORT bool dsn_register_app_role_helper(const char* type_name, const char* create, const char* start, dsn_app_destroy destroy);
+	EXPORT void dsn_run_helper(int argc, char** argv, bool sleep_after_init);
+	EXPORT void dsn_task_call_helper(dsn_task_t task, dsn_task_tracker_t tracker, int delay_milliseconds);
+	EXPORT dsn_task_t dsn_task_create_helper(dsn_task_code_t code, void* param, int hash);
+	EXPORT dsn_task_t dsn_task_create_timer_helper(dsn_task_code_t code, void* param, int hash, int interval_milliseconds);
+	EXPORT uint64_t dsn_address_build_helper(const char* host, int port);
+	EXPORT void dsn_rpc_call_helper(uint64_t addr, dsn_task_t rpc_call, dsn_task_tracker_t tracker);
+	EXPORT void* dsn_rpc_call_wait_helper(uint64_t addr, dsn_message_t msg, char *ss);
+	EXPORT dsn_task_t dsn_rpc_create_response_task_helper(dsn_message_t msg, int param, int reply_hash);
+	EXPORT dsn_message_t dsn_msg_create_request_helper(dsn_task_code_t code, int timeout_milliseconds, int request_hash);
+	EXPORT dsn_task_code_t dsn_task_code_register_helper(const char* name, int type, int pri, dsn_threadpool_code_t pool);
+	EXPORT void marshall_helper(dsn_message_t msg, char* request_content);
+	EXPORT bool dsn_rpc_register_handler_helper(dsn_task_code_t code, const char* name, int param);
+	EXPORT void marshall_int_msg_helper(int msg_int, char* request_content);
 };
 
+
+static __thread struct __tls_handler_function__
+{
+	int magic;
+	PyObject *rpc_request_function;
+	PyObject *timer_function;
+	PyObject *task_function;
+} tls_handler_function;
+
+PyObject* tls_get_handler_function(std::string handler_name)
+{
+	if (tls_handler_function.magic != 0xdeadbeef)
+	{
+		// initialization goes here
+		tls_handler_function.magic = 0xdeadbeef;
+		PyGILState_STATE gstate;
+		gstate = PyGILState_Ensure();
+		
+		// on_request_handler
+		PyObject* pFunc1 = NULL;
+		PyObject* pModule1 = NULL;
+		PyObject* pDict1 = NULL;
+		PyObject* pClass1 = NULL;
+		pModule1 = PyImport_ImportModule("dev.python.InterOpLookupTable");
+		pDict1 = PyModule_GetDict(pModule1);
+		pClass1 = PyDict_GetItemString(pDict1, "InterOpLookupTable");
+		pFunc1 = PyObject_GetAttrString(pClass1, "on_request_handler");
+		tls_handler_function.rpc_request_function = pFunc1;
+
+		// on_timer_handler
+		PyObject* pFunc2 = NULL;
+		PyObject* pModule2 = NULL;
+		PyObject* pDict2 = NULL;
+		PyObject* pClass2 = NULL;
+		pModule2 = PyImport_ImportModule("dev.python.InterOpLookupTable");
+		pDict2 = PyModule_GetDict(pModule2);
+		pClass2 = PyDict_GetItemString(pDict2, "InterOpLookupTable");
+		pFunc2 = PyObject_GetAttrString(pClass2, "on_timer_handler");
+		tls_handler_function.timer_function = pFunc2;
+		
+		// task_handler
+		PyObject* pFunc3 = NULL;
+		PyObject* pModule3 = NULL;
+		PyObject* pDict3 = NULL;
+		PyObject* pClass3 = NULL;
+		pModule3 = PyImport_ImportModule("dev.python.InterOpLookupTable");
+		pDict3 = PyModule_GetDict(pModule3);
+		pClass3 = PyDict_GetItemString(pDict3, "InterOpLookupTable");
+		pFunc3 = PyObject_GetAttrString(pClass3, "task_handler");
+		tls_handler_function.task_function = pFunc3;
+
+		PyGILState_Release(gstate);
+	}
+	if (handler_name == "on_request_handler")
+		return tls_handler_function.rpc_request_function;
+	else if (handler_name == "on_timer_handler")
+		return tls_handler_function.timer_function;
+	else if (handler_name == "task_handler")
+		return tls_handler_function.task_function;
+	else
+		perror("handler function not found");
+	return nullptr;
+}
 
 void dsn_run_helper(int argc, char** argv, bool sleep_after_init)
 {
@@ -162,23 +233,13 @@ static void task_handler(void *param)
 	PyGILState_STATE gstate;
 	gstate = PyGILState_Ensure();
 
-	PyObject* pModule = NULL;
-	PyObject* pDict = NULL;
-	PyObject* pClass = NULL;
-	PyObject* pFunc = NULL;
-	PyObject * pArgs = NULL;
-	PyObject * pReturn = NULL;
-	pModule = PyImport_ImportModule("dev.python.InterOpLookupTable");
-	pDict = PyModule_GetDict(pModule);
-	pClass = PyDict_GetItemString(pDict, "InterOpLookupTable");
-	pFunc = PyObject_GetAttrString(pClass, "task_handler");
-
-	pArgs = PyTuple_New(1);
+	PyObject* pArgs = PyTuple_New(1);
 	PyTuple_SetItem(pArgs, 0, Py_BuildValue("i", (int)param));
-	pReturn = PyEval_CallObject(pFunc, pArgs);
-
-	// task handler end
+	PyObject* pFunc = tls_get_handler_function("task_handler");
+	PyObject* pReturn = PyEval_CallObject(pFunc, pArgs);
+	
 	PyGILState_Release(gstate);
+	// task handler end
 	return;
 }
 
@@ -200,50 +261,16 @@ void dsn_task_call_helper(dsn_task_t task, dsn_task_tracker_t tracker, int delay
 // rpc helper
 uint64_t dsn_address_build_helper(const char* host, int port)
 {
-	return *(uint64_t*)&dsn_address_build(host, port);
-}
-
-static __thread struct __tls_timer_function__
-{
-	int magic;
-	PyObject *timer_function;
-} tls_timer_function;
-
-PyObject* tls_get_rpc_response_function()
-{
-	if (tls_timer_function.magic != 0xdeadbeef)
-	{
-		// initialization goes here
-		tls_timer_function.magic = 0xdeadbeef;
-		PyGILState_STATE gstate;
-		gstate = PyGILState_Ensure();
-
-		PyObject* pFunc = NULL;
-		PyObject* pModule = NULL;
-		PyObject* pDict = NULL;
-		PyObject* pClass = NULL;
-		PyObject * pArgs = NULL;
-		PyObject * pReturn = NULL;
-		pModule = PyImport_ImportModule("dev.python.InterOpLookupTable");
-		pDict = PyModule_GetDict(pModule);
-		pClass = PyDict_GetItemString(pDict, "InterOpLookupTable");
-		pFunc = PyObject_GetAttrString(pClass, "on_timer_handler");
-		tls_timer_function.timer_function = pFunc;
-		PyGILState_Release(gstate);
-	}
-	return tls_timer_function.timer_function;
+	dsn_address_t addr = dsn_address_build(host, port);
+	return *(uint64_t*)&addr;
 }
 
 // client register rpc callback
 static void rpc_response_handler(dsn_error_t err, dsn_message_t rpc_request, dsn_message_t rpc_response, void* param)
 {
-	// acquire Python's Global Interpreter Lock (GIL) before calling any Python API functions
+	// read response
 	PyGILState_STATE gstate;
 	gstate = PyGILState_Ensure();
-
-	PyObject* pFunc = tls_get_rpc_response_function();
-	PyObject * pArgs = NULL;
-	PyObject * pReturn = NULL;
 
 	void* ptr;
 	size_t size;
@@ -252,10 +279,11 @@ static void rpc_response_handler(dsn_error_t err, dsn_message_t rpc_request, dsn
 	memcpy_s(buffer, size, ptr, size);
 	dsn_msg_read_commit(rpc_response, size);
 
-	pArgs = PyTuple_New(2);
+	PyObject* pArgs = PyTuple_New(2);
 	PyTuple_SetItem(pArgs, 0, Py_BuildValue("s", buffer));
 	PyTuple_SetItem(pArgs, 1, Py_BuildValue("i", (int)param));
-	pReturn = PyEval_CallObject(pFunc, pArgs);
+	PyObject* pFunc = tls_get_handler_function("on_timer_handler");
+	PyObject* pReturn = PyEval_CallObject(pFunc, pArgs);
 
 	PyGILState_Release(gstate);
 	return;
@@ -308,48 +336,12 @@ void marshall_int_msg_helper(int msg_int, char* request_content)
 	dsn_msg_write_commit(msg, count);
 }
 
-static __thread struct __tls_rpc_request_function__
-{
-	int magic;
-	PyObject *rpc_request_function;
-} tls_rpc_request_function;
-
-PyObject* tls_get_rpc_request_function()
-{
-	if (tls_rpc_request_function.magic != 0xdeadbeef)
-	{
-		// initialization goes here
-		tls_rpc_request_function.magic = 0xdeadbeef;
-		PyGILState_STATE gstate;
-		gstate = PyGILState_Ensure();
-
-		PyObject* pFunc = NULL;
-		PyObject* pModule = NULL;
-		PyObject* pDict = NULL;
-		PyObject* pClass = NULL;
-		PyObject * pArgs = NULL;
-		PyObject * pReturn = NULL;
-		pModule = PyImport_ImportModule("dev.python.InterOpLookupTable");
-		pDict = PyModule_GetDict(pModule);
-		pClass = PyDict_GetItemString(pDict, "InterOpLookupTable");
-		pFunc = PyObject_GetAttrString(pClass, "on_request_handler");
-		tls_rpc_request_function.rpc_request_function = pFunc;
-		PyGILState_Release(gstate);
-	}
-	return tls_rpc_request_function.rpc_request_function;
-}
-
 // server register handler
 static void rpc_request_handler(dsn_message_t rpc_request, void* param)
 {
 	// rpc request handler start
-	// acquire Python's Global Interpreter Lock (GIL) before calling any Python API functions
 	PyGILState_STATE gstate;
 	gstate = PyGILState_Ensure();
-
-	PyObject* pFunc = tls_get_rpc_request_function();
-	PyObject * pArgs = NULL;
-	PyObject * pReturn = NULL;
 
 	void* ptr;
 	size_t size;
@@ -360,15 +352,14 @@ static void rpc_request_handler(dsn_message_t rpc_request, void* param)
 
 	dsn_message_t rpc_response = dsn_msg_create_response(rpc_request);
 
-	pArgs = PyTuple_New(3);
+	PyObject* pArgs = PyTuple_New(3);
 	PyTuple_SetItem(pArgs, 0, Py_BuildValue("s", buffer));
 	PyTuple_SetItem(pArgs, 1, Py_BuildValue("i", (int)rpc_response));
 	PyTuple_SetItem(pArgs, 2, Py_BuildValue("i", (int)param));
-	pReturn = PyEval_CallObject(pFunc, pArgs);
+	PyObject* pFunc = tls_get_handler_function("on_request_handler");
+	PyObject* pReturn = PyEval_CallObject(pFunc, pArgs);
 
-	// rpc request handler end
 	PyGILState_Release(gstate);
-
 	return;
 }
 
