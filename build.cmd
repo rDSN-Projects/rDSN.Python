7a7a49ed071bcafa4de9d52c1d268803bb70b5de
mcfatealan <louchang_new@163.com>
2015-12-21 12:50:24 +0800

    modify build.cmd using msbuild

--- build.cmd
+++ build.cmd
@@ -1,12 +1,50 @@
-set /p DUMMY=[rDSN.Python] please run C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat amd64 first
-mkdir builder
-cd builder
-echo "[rDSN.Python] cmake"
-cmake .. -G "NMake Makefiles" || echo "cmake failed!" && exit /b
-echo "[rDSN.Python] nmake"
-nmake || echo "nmake failed!" && exit /b
-echo "[rDSN.Python] nmake install"
-nmake install || echo "nmake install failed!" && exit /b 
+SET TOP_DIR=%~dp0
+SET bin_dir=%TOP_DIR%\scripts\windows
+SET build_type=%1
+SET build_dir=%~f2
+
+IF "%build_type%" EQU "" SET build_type=Debug
+
+IF "%build_dir%" EQU "" (
+    CALL %bin_dir%\echoc.exe 4 please specify build_dir
+    GOTO error
+)
+
+IF NOT "%VS140COMNTOOLS%"=="" (
+    CALL "%VS140COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
+    SET cmake_target=Visual Studio 14 2015 Win64
+) ELSE (
+    IF NOT "%VS120COMNTOOLS%"=="" (
+        CALL "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" amd64
+        SET cmake_target=Visual Studio 12 2013 Win64
+    )
+)
+
+IF "%cmake_target%"=="" (
+    ECHO "error: Visusal studio 2013 or 2015 is not installed, please fix and try later"
+    GOTO error
+)
+
+IF NOT EXIST "%bin_dir%\7z.exe" (
+    CALL %bin_dir%\wget.exe --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/7z.dll?raw=true
+    CALL %bin_dir%\wget.exe --no-check-certificate https://github.com/imzhenyu/packages/raw/master/windows/7z.exe?raw=true
+    @move 7z.dll %bin_dir%
+    @move 7z.exe %bin_dir%
+)
+
+IF NOT EXIST "%TOP_DIR%\ext\cmake-3.2.2" (
+    CALL %bin_dir%\wget.exe --no-check-certificate http://github.com/imzhenyu/packages/blob/master/windows/cmake-3.2.2.7z?raw=true
+    CALL %bin_dir%\7z.exe x cmake-3.2.2.7z -y -o"%TOP_DIR%\ext\"
+)
+
+IF NOT EXIST "%build_dir%" mkdir %build_dir%
+
+cd /d %build_dir%
+
+echo CALL %TOP_DIR%\ext\cmake-3.2.2\bin\cmake.exe .. -DCMAKE_BUILD_TYPE="%build_type%" -G "%cmake_target%"
+CALL %TOP_DIR%\ext\cmake-3.2.2\bin\cmake.exe .. -DCMAKE_BUILD_TYPE="%build_type%" -G "%cmake_target%"
+
+msbuild dsn.python.sln /p:Configuration=%build_type%
 
 cd ..\src
 echo "[rDSN.Python] install help functions in python"
@@ -17,3 +55,11 @@ python pip_install_for_windows.py || echo "install pip failed!" && exit /b
 cd apps\rDSN.monitor
 echo "[rDSN.Python] install dependency packages" 
 pip install -r requirement.txt || echo "install dependency packages failed!" && exit /b
+
+cd ..\..\..
+goto exit
+
+:error
+    CALL %bin_dir%\echoc.exe 4  "Usage: build.cmd build_type(Debug|Release|RelWithDebInfo|MinSizeRel) build_dir"
+
+:exit
