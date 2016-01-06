649af03babf076b6f836d1cf268fd8f214014ca2
mcfatealan <louchang_new@163.com>
2016-01-06 09:50:27 +0800

    Finish realtime-analyzer without linearization

--- src/apps/rDSN.monitor/app_package/static/js/view.js
+++ src/apps/rDSN.monitor/app_package/static/js/view.js
@@ -92,21 +92,57 @@ function barUpdateData(counterList)
     }, interval * 1000);
 }
 
+//time operation
+function strtoms(str){
+        var arr = str.split(":");
+        var arr2 = arr[2].split(".");
+        return ((Number(arr[0]) * 60 + Number(arr[1])) * 60 + Number(arr2[0])) * 1000 + Number(arr2[1]);
+}
 
+function mstostr(milli){
+        var hr= ("00" + Math.floor((milli / (60 * 60 * 1000))%100).toString()).substr(-2,2);
+        var min= ("00" + Math.floor((milli / (60 * 1000))%60).toString()).substr(-2,2);
+        var sc= ("00" + Math.floor((milli / 1000)%60).toString()).substr(-2,2);
+        var ms=  ("00" + (milli %1000).toString()).substr(-3,3);
+        return hr + ":" + min + ":" + sc + "." + ms; 
+}
+
+function calcdiffms(time1,time2){
+        
+    var res = strtoms(time2) - strtoms(time1);
+    if (res<0)
+        return (res + 24*60*60*1000);
+    return res;
+}
+
+function addmstostr(str,ms){
+    return mstostr(strtoms(str)+ms);
+}
+
+var latestdata, latesttime;
+    var lastdata, lasttime;
+    var DataToUpdate;
+    var inittime;
 
 
 function realtimeDisplay(){
     var basedata = [
         ['x', '1', '2', '3','4','5','6','7','8','9','10', '11', '12', '13','14','15','16','17','18','19'],
     ];
-    var latestdata=[['x', 1]];
+    
+
     var counterList = JSON.parse(getParameterByName('counterList'));
-    var tabledata=[]
+    
+    DataToUpdate = [['x', 1]];
+    latestdata=[]; latesttime=[];
+    lastdata=[]; lasttime=[];
+    inittime = 0;
+
     for (counter in counterList)
     {
         var name = counterList[counter]['name'];
         basedata.push([name,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
-        latestdata.push([name, 0]);
+        DataToUpdate.push([name, 0]);
     }
 
     chart = c3.generate({
@@ -121,17 +157,21 @@ function realtimeDisplay(){
         }
     });
 
-    $("#realtimeData thead").append("<tr><th>Counter Name</th><th>Counter Value</th></tr>");
+    $("#realtimeData thead").append("<tr><th>Counter Name</th><th>Counter Value</th><th>rDSN Time</th></tr>");
     for (counter in counterList)
     {
-        $("#realtimeData tbody").append("<td>" + counterList[counter]['name'] + "</td><td><span class='counter" + counter + "'></td>");
+        $("#realtimeData tbody").append("<td>" + counterList[counter]['name'] + "</td><td><span class='counter" + counter + "'></td><td><span class='time" + counter + "'></td>");
     }
 
-    realtimeUpdateData(20,counterList,latestdata);
+    $("#baseTimeData thead").append("<tr><th>Base Time</th></tr>");
+    $("#baseTimeData tbody").append("<td><span class='basetime'></td>");
+
+    inittime = (new Date()).getTime();
+    realtimeUpdateData(20,counterList);
 
 }
 
-function realtimeUpdateData(a,counterList,latestdata)
+function realtimeUpdateData(a,counterList)
 {
     var ajaxcount=0;
     var length = counterList.length;
@@ -147,20 +187,33 @@ function realtimeUpdateData(a,counterList,latestdata)
                 commands: JSON.stringify(["pq time","counter." + graphtype + "i " + index])
             }, function(data){
                 data = JSON.parse(data);
-                latestdata[Number(counter)+1][1]=data[1];
                 $('.counter'+counter).html(data[1]);
+                $('.time'+counter).html(data[0]);
+
+                updateRefData(counter,data[0],data[1]);
+                
             });
         })(counter);
     }
+    if (latesttime[0] != undefined)
+    {
+        $('.basetime').html(addmstostr(latesttime[0], (new Date()).getTime() - inittime));
+    }
+
     chart.flow({
-        columns: latestdata,
-        duration:1000,
+        columns: DataToUpdate,
+        duration:interval*1000,
         done:function(){
             setTimeout(function () {
-                realtimeUpdateData(a+1, counterList, latestdata);
-            }, interval*1000);}
+                //chart.xgrids.add([{value: a, text:time,class:'hoge'}]);
+                realtimeUpdateData(a+1, counterList);
+            }, 0);}
     });
 }
 
+function updateRefData(counter,time,val){
+    latestdata[counter]=val;
+    latesttime[counter]=time;
+}
     
 
