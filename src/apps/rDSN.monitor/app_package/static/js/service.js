cde79fb65ec4e54f1b897d4ac6e10a0b80afe059
ykwd <oneday117@qq.com>
2016-01-21 13:27:03 +0800

    fix a bug of monitor; add new feature of updeploy"

--- src/apps/rDSN.monitor/app_package/static/js/service.js
+++ src/apps/rDSN.monitor/app_package/static/js/service.js
@@ -22,10 +22,10 @@ function updateTable()
             data = JSON.parse(data)['services'];
             result = "";
             
-            $("#serviceList tbody > tr").empty();
+            $("#serviceList tbody").empty();
             for(var service in data)
             {
-                result = '<tr id="' + data[service]['name'] + '"><td>' + data[service]['name'] + '</td><td>' + data[service]['package_id'] + '</td><td>' + data[service]['cluster'] + '</td><td>' + data[service]['service_url'] + '</td><td>' + data[service]['status'] + '</td><td>' + data[service]['error'] + '</td><td><span class="glyphicon glyphicon-remove" aria-hidden="true" onclick="undeploy("' + data[service]['name'] + '");"></span></td></tr>';
+                result = '<tr id="' + data[service]['name'] + '"><td>' + data[service]['name'] + '</td><td>' + data[service]['package_id'] + '</td><td>' + data[service]['cluster'] + '</td><td>' + data[service]['service_url'] + '</td><td>' + data[service]['status'] + '</td><td>' + data[service]['error'] + '</td><td><span class="glyphicon glyphicon-remove" aria-hidden="true" onclick="undeploy(\'' + data[service]['name'] + '\');"></span></td></tr>';
                 $("#serviceList tbody").append(result);
             }
             window.setTimeout(function () {
@@ -40,8 +40,9 @@ function updateTable()
 }
 
 function undeploy(service_name) {
+    req = {"service_name":service_name};
     $.post("/api/pack/undeploy", { 
-        service_name:service_name
+        command:"server.undeploy " + JSON.stringify(req)
         }, function(data){ 
             if(data == 'ERR_OK')
             {
